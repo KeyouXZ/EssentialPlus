daa5e59c433d66e1715ab10e13e9d33cc16cbe04
Keyou <keyouz@outlook.com>
2024-10-21 19:58:06 +0700

    Translating Config.cs

---
+++ EssentialPlus/Config.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+
+namespace EssentialsPlus;
+
+public class Config
+{
+    [JsonProperty("Pvp Disable Command")]
+    public string[] DisabledCommandsInPvp = new string[]
+    {
+"eback"
+    };
+
+    [JsonProperty("Back Position History")]
+    public int BackPositionHistory = 10;
+
+    [JsonProperty("MySql Host")]
+    public string MySqlHost = "";
+
+    [JsonProperty("MySql Database Name")]
+    public string MySqlDbName = "";
+
+    [JsonProperty("MySql Username")]
+    public string MySqlUsername = "";
+
+    [JsonProperty("MySql Password")]
+    public string MySqlPassword = "";
+
+    public void Write(string path)
+    {
+        File.WriteAllText(path, JsonConvert.SerializeObject(this, Formatting.Indented));
+    }
+    public static Config Read(string path) { return File.Exists(path) ? JsonConvert.DeserializeObject<Config>(File.ReadAllText(path)) : new Config(); }
+}
